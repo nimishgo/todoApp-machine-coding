a7839090f053ccf6d8e402dcdf5f0e9736ca20dc
nimishgo <nimish.2711@gmail.com>
2024-06-26 22:08:00 +0530

    doing with reducers and context

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useReducer } from "react";
 import AddForm from "./components/AddForm";
 import ShowList from "./components/ShowList";
 import taskReducer, { initialState } from "./utils/reducers";
+import { listDispatch, listStore } from "./utils/itemStore";
 function App() {
   // const [list, setList] = useState([]);
   const [list, dispatch] = useReducer(taskReducer, initialState);
@@ -45,11 +46,19 @@ function App() {
   };
   return (
     <>
-      <div className="h-[50vh] flex flex-col justify-center items-center ">
-        <h1>To Do list</h1>
-        <AddForm addTask={addTask} />
-        <ShowList list={list} removeTask={removeTask} modifyTask={modifyTask} />
-      </div>
+      <listStore.Provider value={list}>
+        <listDispatch.Provider value={dispatch}>
+          <div className="h-[50vh] flex flex-col justify-center items-center ">
+            <h1>To Do list</h1>
+            <AddForm addTask={addTask} />
+            <ShowList
+              list={list}
+              removeTask={removeTask}
+              modifyTask={modifyTask}
+            />
+          </div>
+        </listDispatch.Provider>
+      </listStore.Provider>
     </>
   );
 }
